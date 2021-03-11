98f53ebc9d1e7d10f210079a2707a241ca3f6004
Sandy Dai <sandydai98@gmail.com>
2021-03-11 17:56:01 -0500

    feat(js/main): message clears after sending

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,32 @@
 // Accesses chat form
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
 // Message received from server
 socket.on('message', (message) => {
-  console.log(message);
-
+  // Function that puts message into DOM chat box
   outputMessage(message);
+
+  // Scrolls down every time we get a message
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
-// Message submit
+// Event Listener on Chat Message Form
 chatForm.addEventListener('submit', (evt) => {
   // prevents form from refreshing after submitting
   evt.preventDefault();
 
-  // Gets by input id, "msg"
+  // Gets message value by input id, "msg"
   const msg = evt.target.elements.msg.value;
 
   // Emits input message to server
   socket.emit('chatMessage', msg);
+
+  // Clears input message
+  evt.target.elements.msg.value = '';
+  evt.target.elements.msg.focus();
 });
 
 // Output message to DOM
@@ -29,6 +36,6 @@ function outputMessage(message) {
   div.classList.add('message');
   div.innerHTML = `<p class="meta" id="name">Brandon <span>9:12pm</span></p>
   <p class="text">${message}</p>`;
-  // Find chat-messages class and add the div to it
+  // Find "chat-messages" class and add the div to it
   document.querySelector('.chat-messages').appendChild(div);
 }
